3d52fed3fb968c35fda9e12aefd09826e8d40e21
DanielDev-java <manodaniel429@gmail.com>
2024-12-23 11:54:01 -0300

    Repository e Config

---
+++ src/main/java/com/workshopspring/curso/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.workshopspring.curso.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.workshopspring.curso.entities.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
