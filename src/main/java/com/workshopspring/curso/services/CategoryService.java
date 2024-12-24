aa589caca5da4e4bd491efdf5c06727493febbf7
DanielDev-java <manodaniel429@gmail.com>
2024-12-24 09:28:13 -0300

    Entidade Category

---
+++ src/main/java/com/workshopspring/curso/services/CategoryService.java
@@ -0,0 +1,26 @@
+package com.workshopspring.curso.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.workshopspring.curso.entities.Category;
+import com.workshopspring.curso.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+    @Autowired
+    private CategoryRepository repository;
+
+    public List<Category> findAll() {
+        return repository.findAll();
+    }
+
+    public Category findById(Long id) {
+        Optional<Category> category = repository.findById(id);
+        return category.get();
+    }
+
+}
