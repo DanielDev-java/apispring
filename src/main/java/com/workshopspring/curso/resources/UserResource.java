12fdf8a00905f96206d4f132e055b0fd50ecdab1
DanielDev-java <manodaniel429@gmail.com>
2024-12-25 11:16:48 -0300

    Delete User

--- src/main/java/com/workshopspring/curso/resources/UserResource.java
+++ src/main/java/com/workshopspring/curso/resources/UserResource.java
@@ -12,6 +12,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import com.workshopspring.curso.entities.User;
 import com.workshopspring.curso.services.UserService;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -41,6 +42,12 @@ public class UserResource {
         user = service.insert(user);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
         return ResponseEntity.created(uri).body(user);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
     }
     
 }
